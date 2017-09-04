fa50a52525bf44cf48aeac27e98d9117d0e4396a
Jackson Callaway <jcksncllwy@gmail.com>
2017-09-03 21:03:58 -0700

    another pixel color method

--- neccesary-disorder/main.js
+++ neccesary-disorder/main.js
@@ -1,15 +1,17 @@
-var margin = 100
-var weAreGiffing = false
-var numFrames = 20
+var margin = 0
+var weAreGiffing = true
+var numFrames = 48
 var frameCount = 0
+var canvasSize;
 
 
 function setup(){
-	sz = min(windowWidth, windowHeight)
-  createCanvas(sz, sz)
+	canvasSize = min(windowWidth, windowHeight)
+  createCanvas(canvasSize, canvasSize)
   if(weAreGiffing){
     startTheGifThing()
   }
+  rectMode(CENTER)
 }
 
 function draw(){
@@ -21,15 +23,17 @@ function draw(){
   // Draws every pixel
   for(var i=margin;i<width-margin;i++){
     for(var j=margin;j<height-margin;j++){
-      stroke(pixelColor(i,j,t))
+      stroke(pixelColorMethods[0](i,j,t))
       point(i,j)
     }
   }
-
+  
   // Draws a white rectangle
-  stroke(255)
+  stroke(255) 
   noFill()
-  rect(margin,margin,width-2*margin,height-2*margin)  
+  var csz = min(windowWidth, windowHeight)
+  var size = csz-(margin*2)
+  //rect(csz/2, csz/2, size, size )
 
 	if(weAreGiffing){
 		encoder.addFrame(context)
@@ -40,45 +44,79 @@ function draw(){
 	}
 }
 
-function pixelColor(x,y,t){
-  var scalarOffset1 = noiseScalarFieldOffset(x,y, 20,0.03,0)
-  var waveform1 = sin(TWO_PI*(t+scalarOffset1))
-  var waveZeroToOne1 = map(waveform1,-1,1,0,1)
-  var easedWave1 = ease(waveZeroToOne1, 10)
+var pixelColorMethods = [
+  function(x,y,t){
+    
+  },
+  function (x,y,t){
+    var baseNoisePower = 11
 
-  var scalarOffset2 = noiseScalarFieldOffset(x,y, 20,0.03,0.1)
-  var waveform2 = sin(TWO_PI*(t+scalarOffset2))
-  var waveZeroToOne2 = map(waveform2,-1,1,0,1)
-  var easedWave2 = ease(waveZeroToOne2, 10)
-  
-  var r1 = 255*waveZeroToOne1
-  var g1 = 255*(1-waveZeroToOne1)
-  var b1 = 0
+    var scalarOffset1 = centeredNoiseScalarOffset(canvasSize/2,canvasSize/2, x,y, 0.002, baseNoisePower)
+    var waveform1 = sin(TWO_PI*(t+scalarOffset1))
+    var waveZeroToOne1 = map(waveform1,-1,1,0,1)
+    
 
-  var r2 = 255*waveZeroToOne2
-  var g2 = 0
-  var b2 = 255*(1-waveZeroToOne2)
+    var scalarOffset2 = centeredNoiseScalarOffset(canvasSize/2,canvasSize/2, x,y, 0.002, baseNoisePower+0.5)
+    var waveform2 = sin(TWO_PI*(t+scalarOffset2))
+    var waveZeroToOne2 = map(waveform2,-1,1,0,1)
+    
 
-  return color((r1+r2), (g1+g2), (b1+b2))
-}
+    var scalarOffset3 = centeredNoiseScalarOffset(canvasSize/2,canvasSize/2, x,y, 0.002, baseNoisePower+1)
+    var waveform3 = sin(TWO_PI*(t+scalarOffset3))
+    var waveZeroToOne3 = map(waveform3,-1,1,0,1)
+    
+    var distance = dist(canvasSize/2,canvasSize/2,x,y)
+    var distanceColorDampening = map(ease(map(distance, 0,canvasSize/2,0,1), 5),0,1,1,0)
+    return color(255*waveZeroToOne1*distanceColorDampening, 255*waveZeroToOne2*distanceColorDampening, 255*waveZeroToOne3*distanceColorDampening)
+  },
 
-function noisePixelColor(x,y,t){
-  var scalarOffset1 = noiseScalarFieldOffset(x,y,20,0.003,1)
-  var waveform1 = sin(TWO_PI*(t+scalarOffset1))
-  var waveZeroToOne1 = map(waveform1,-1,1,0,1)
-  var easedWave1 = ease(waveZeroToOne1, 3)
+  function (x,y,t){
+    var scalarOffset1 = noiseScalarFieldOffset(x,y, 20,0.005,0)
+    var waveform1 = sin(TWO_PI*(t+scalarOffset1))
+    var waveZeroToOne1 = map(waveform1,-1,1,0,1)
+    var easedWave1 = ease(waveZeroToOne1, 10)
 
-  var scalarOffset2 = noiseScalarFieldOffset(x,y,20,0.003,100)
-  var waveform2 = sin(TWO_PI*(t+scalarOffset2))
-  var waveZeroToOne2 = map(waveform2,-1,1,0,1)
-  var easedWave2 = ease(waveZeroToOne2, 3)
+    var scalarOffset2 = noiseScalarFieldOffset(x,y, 40,0.005,0)
+    var waveform2 = sin(TWO_PI*(t+scalarOffset2))
+    var waveZeroToOne2 = map(waveform2,-1,1,0,1)
+    var easedWave2 = ease(waveZeroToOne2, 10)
+    
+    var r1 = 0
+    var g1 = 255*waveZeroToOne1
+    var b1 = 255*(1-waveZeroToOne1)
 
-  var scalarOffset3 = noiseScalarFieldOffset(x,y,20,0.003,200)
-  var waveform3 = sin(TWO_PI*(t+scalarOffset3))
-  var waveZeroToOne3 = map(waveform3,-1,1,0,1)
-  var easedWave3 = ease(waveZeroToOne3, 3)
-  
-  return color(255*easedWave1, 255*easedWave2, 255*easedWave3)
+    var r2 = 255*waveZeroToOne2
+    var g2 = 0
+    var b2 = 255*(1-waveZeroToOne2)
+
+    return color((r1+r2), (g1+g2), (b1+b2))
+  },
+  function (x,y,t){
+    var scalarOffset1 = noiseScalarFieldOffset(x,y,20,0.003,1)
+    var waveform1 = sin(TWO_PI*(t+scalarOffset1))
+    var waveZeroToOne1 = map(waveform1,-1,1,0,1)
+    var easedWave1 = ease(waveZeroToOne1, 3)
+
+    var scalarOffset2 = noiseScalarFieldOffset(x,y,20,0.003,100)
+    var waveform2 = sin(TWO_PI*(t+scalarOffset2))
+    var waveZeroToOne2 = map(waveform2,-1,1,0,1)
+    var easedWave2 = ease(waveZeroToOne2, 3)
+
+    var scalarOffset3 = noiseScalarFieldOffset(x,y,20,0.003,200)
+    var waveform3 = sin(TWO_PI*(t+scalarOffset3))
+    var waveZeroToOne3 = map(waveform3,-1,1,0,1)
+    var easedWave3 = ease(waveZeroToOne3, 3)
+    
+    return color(255*easedWave1, 255*easedWave2, 255*easedWave3)
+  },
+]
+
+function centeredNoiseScalarOffset(centerX,centerY, x,y, noiseScale,noiseIntensity){
+  distance = dist(x,y,centerX,centerY)
+  perlin_noise_intensity = map(ease(map(distance, 0,canvasSize/2,0,1), 5),0,1,1,0)*noiseIntensity
+   
+  result = noise(noiseScale*x,noiseScale*y) * perlin_noise_intensity  
+  return result
 }
 
 function centerScalarOffset(centerX,centerY, x,y){
